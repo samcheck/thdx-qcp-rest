d6739833bedb27b08efd8d5ed21b60e20ad8a595
Samuel Check <scheck@salesforce.com>
2021-06-21 15:56:06 -0500

    add some logs

--- index.js
+++ index.js
@@ -12,24 +12,29 @@ app.use(helmet());
 app.use(bodyParser.json()); // for parsing application/json
 // Serve static files
 app.use(express.static(__dirname + "/public"));
+console.log('after public')
 
 // Routes assignment
 // pricing route
 app.use("/api", require("./routes/price"));
+console.log('after price api')
 
 // Log unhandled Promise rejections
 process.on("unhandledRejection", (reason, p) => {
   console.error(`Unhandled Rejection at: Promise ${p} reason: ${reason}`);
 });
+console.log('after unhandled rejection')
 
 //Handle unknown routes
 app.use((req, res) => {
   console.error("The request didn't match any endpoint");
   return res.status(404).send("404 – Not Found");
 });
+console.log('before sequalize')
 
 // Connect to the database and then Serve the app
 models.sequelize.sync().then(() => {
+  console.log('sequalize sync')
   app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
   });
